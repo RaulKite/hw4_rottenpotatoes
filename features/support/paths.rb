a3aef400c99c9f7f0605558e5333ffb41833c9da
Raul Sanchez <raul@um.es>
2012-03-26 10:51:07 +0200

    Added hw3 scenarios to have more coverage

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -16,6 +16,8 @@ module NavigationHelpers
     when /^the home\s?page$/
       '/movies'
 
+    when /^the RottenPotatoes home page$/
+      'movies'
 
     when /^the (.*) page for "(.*)"$/
       #m = Movie.find_by_title("$1")
